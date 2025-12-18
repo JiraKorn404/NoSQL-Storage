dddeddc64f4ed7e5dc778d9af49aa665229d5997
JiraKorn404 <korn7716@gmail.com>
2025-12-18 17:44:02 +0700

    comment code

--- src/app.py
+++ src/app.py
@@ -5,10 +5,12 @@ import database
 
 app = FastAPI(lifespan=database.lifespan)
 
+# upload file into mongodb
 @app.post('/upload/')
 def upload_pdf(
-    file: UploadFile = File(...)
+    file: UploadFile = File(...) # open for file upload
 ):
+    # get file id
     file_id = database.fs.put(file.file, filename=file.filename)
     return {
         'file_id': str(file_id),
--- src/database.py
+++ src/database.py
@@ -7,18 +7,23 @@ from pymongo.errors import ConnectionFailure
 import gridfs
 from contextlib import asynccontextmanager
 
+# load .env
 load_dotenv(override=True)
 
+# load mongodb environment
 mongodb_url = os.getenv('MONGODB_URL')
 database_name = os.getenv('DATABASE_NAME')
 
+# # define global mongodb variable
 client = None
 fs = None
 
+# main database connection function
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     global client, fs
     try:
+        # test database connection
         client = MongoClient(mongodb_url, serverSelectionTimeoutMS=5000)
         client.admin.command('ping')
         print('Connected to MongoDB succesfully!')
